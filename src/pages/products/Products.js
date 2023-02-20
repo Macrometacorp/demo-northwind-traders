dcfa2e13c57660890f474d56163eb6fc2e9c098d
Justin Johnson <justin@rad.as>
2023-02-20 11:24:48 -0800

    update getting started instructions

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -226,21 +226,16 @@ export function Products() {
                             "products.".
                         </ListItem>
                         <ListItem fontSize="lg">
-                            Import the data from the "products.json" file into
-                            the newly created collection.
+                            Import the data from the "products.json" file located in the `ExtraFiles` folder in this project into
+                            the newly created collection. Before you confirm make sure you select `ProductID` as the key.
                         </ListItem>
                         <ListItem fontSize="lg">
                             Create additional document store collections for
                             "categories" and "suppliers."
                         </ListItem>
                         <ListItem fontSize="lg">
-                            Import categories.json and suppliers.json to the
-                            created collections.
-                        </ListItem>
-                        <ListItem fontSize="lg">
-                            Import the corresponding data from "categories.json"
-                            and "suppliers.json" into their respective
-                            collections.
+                            Import categories.json and suppliers.json files located in the `ExtraFiles` folder in this project to the
+                            created collections. Before you confirm make sure you select `CategoryID` as the key for categories and `SupplierID` as the key for suppliers.
                         </ListItem>
                         <ListItem fontSize="lg">
                             Finally, navigate to the Query Workers section and
